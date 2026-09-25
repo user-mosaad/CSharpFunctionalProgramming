0422d7e4f72a22dd0e293926fdfcf68f5c4c32db
user-mosaad <msaadshahid999@gmail.com>
2026-09-25 15:04:27 +0600

    Write the code

---
+++ CSharpFunctionalProgramming/Person.cs
@@ -0,0 +1,43 @@
+﻿namespace CSharpFunctionalProgramming;
+
+class Person
+{
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+
+    public Person(string fname, string mname, string lname,
+                  string cityName, string stateName)
+    {
+        FirstName = fname;
+        MiddleName = mname;
+        LastName = lname;
+        City = cityName;
+        State = stateName;
+    }
+
+    // Return the first and last name.
+    public void Deconstruct(out string fname, out string lname)
+    {
+        fname = FirstName;
+        lname = LastName;
+    }
+
+    public void Deconstruct(out string fname, out string mname, out string lname)
+    {
+        fname = FirstName;
+        mname = MiddleName;
+        lname = LastName;
+    }
+
+    public void Deconstruct(out string fname, out string lname,
+                            out string city, out string state)
+    {
+        fname = FirstName;
+        lname = LastName;
+        city = City;
+        state = State;
+    }
+}
--- CSharpFunctionalProgramming/Program.cs
+++ CSharpFunctionalProgramming/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace CSharpFunctionalProgramming;
+
+class Program
+{
+    static void Main()
+    {
+        // 1. Explicit declaration of types
+        (string city, int population, double area) = QueryCityData("New York City");
+        // 2. Using 'var'
+        var (cityy, populationy, areay) = QueryCityData("New York City");
+
+        // Person class
+        var p = new Person("John", "Quirky", "Adams", "Boston", "MA");
+        var (fName, lName, cityyy, state) = p;
+        Console.WriteLine($"Hello {fName} {lName} of {city}, {state}!");
+    }
+
+    private static (string, int, double) QueryCityData(string name)
+    {
+        if (name == "New York City")
+        {
+            return (name, 8_175_133, 468.48);
+        }
+        return ("", 0, 0);
+    }
+}
